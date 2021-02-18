5d368083474e73670ad30854144cd12f1354d897
Milind <vaidyamilind2399@gmail.com>
2021-02-19 00:23:14 +0530

    Backend os condition removed

--- Backend/main.go
+++ Backend/main.go
@@ -6,9 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"runtime"
 
-	"github.com/gen2brain/beeep"
 	"github.com/joho/godotenv"
 )
 
@@ -21,29 +19,33 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	//Desktop Notification pop differnt for differ system
-	if runtime.GOOS == "windows" {
-		executeWindow()
-		//process.Worker()
-	} else {
-		flag.Parse()
-		log.SetFlags(0)
-		http.HandleFunc("/echo", controller.Echo)
-		http.HandleFunc("/", controller.HomePage)
-		fmt.Println("Listening")
-		log.Fatal(http.ListenAndServe(*addr, nil))
-		//service.GetDetailForUser("milind")
-		//userLogin()
-	}
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/echo", controller.Echo)
+	http.HandleFunc("/", controller.HomePage)
+	fmt.Println("Listening")
+	log.Fatal(http.ListenAndServe(*addr, nil))
+
 }
 
+// ---INcase want desktop notification instead of browser based
+//Desktop Notification pop differnt for differ system
+// if runtime.GOOS == "windows" {
+// 	executeWindow()
+// 	//process.Worker()
+// } else {
+
+// 	//service.GetDetailForUser("milind")
+// 	//userLogin()
+// }
+
 //executeWindow is used to send notification in windows machine using beep lib
-func executeWindow() {
-	err := beeep.Notify("Title", "Message body", "assets/information.png")
-	if err != nil {
-		panic(err)
-	}
-}
+// func executeWindow() {
+// 	err := beeep.Notify("Title", "Message body", "assets/information.png")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
 
 //go build -o main.sh
 //chmod +x filename.sh
